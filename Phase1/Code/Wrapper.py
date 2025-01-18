917b450268cbb71405426887bde95316f538c0ec
Colin Balfour <75389927+ColinBalfour@users.noreply.github.com>
2025-01-18 18:38:26 -0500

    initial commit, feature matching done, started RANSAC

---
+++ Phase1/Code/Wrapper.py
@@ -0,0 +1,267 @@
+#!/usr/bin/evn python
+
+"""
+RBE/CS Fall 2022: Classical and Deep Learning Approaches for
+Geometric Computer Vision
+Project 1: MyAutoPano: Phase 2 Starter Code
+
+
+Author(s):
+Lening Li (lli4@wpi.edu)
+Teaching Assistant in Robotics Engineering,
+Worcester Polytechnic Institute
+"""
+
+# Code starts here:
+
+import numpy as np
+import cv2
+
+# Add any python libraries here
+import matplotlib.pyplot as plt
+from copy import deepcopy
+import scipy.ndimage as ndimage
+from skimage.feature import peak_local_max
+
+def main():
+    # Add any Command Line arguments here
+    # Parser = argparse.ArgumentParser()
+    # Parser.add_argument('--NumFeatures', default=100, help='Number of best features to extract from each image, Default:100')
+
+    # Args = Parser.parse_args()
+    # NumFeatures = Args.NumFeatures
+    
+    path = "Set1"
+
+    """
+    Read a set of images for Panorama stitching
+    """
+    im_set = [cv2.imread(f'Phase1/Data/Train/{path}/{i+1}.jpg')for i in range(3)]
+    
+    for im in im_set:
+        cv2.imshow('image', im)
+        cv2.waitKey(2000) 
+
+    """
+	Corner Detection
+	Save Corner detection output as corners.png
+	"""
+    
+    corner_im = []
+    for i, im in enumerate(im_set):
+        gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+        harris_corners = cv2.cornerHarris(gray, 3, 3, 0.04)
+        corner_im.append(harris_corners)
+        # print(harris_corners.shape, im.shape)
+        
+        #result is dilated for marking the corners, not important
+        dst = cv2.dilate(harris_corners,None)
+        
+        # Threshold for an optimal value, it may vary depending on the image.
+        harris_im = deepcopy(im)
+        harris_im[dst>0.01*dst.max()] = [0,0,255]
+        
+        # plt.imshow(harris_corners, cmap='gray')
+        # plt.show()
+        cv2.imwrite(f'outputs/{path}/corners{i}.png', harris_im)
+        
+        
+        ### Subpixel accuracy ###
+        # ret, dst = cv2.threshold(dst,0.01*dst.max(),255,0)
+        # dst = np.uint8(dst)
+        
+        # # find centroids
+        # ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
+        
+        # # define the criteria to stop and refine the corners
+        # criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
+        # corners = cv2.cornerSubPix(gray,np.float32(centroids),(5,5),(-1,-1),criteria)
+        
+        # # Now draw them
+        # res = np.hstack((centroids,corners))
+        # res = np.int0(res)
+        # im[res[:,1],res[:,0]]=[0,0,255]
+        # im[res[:,3],res[:,2]] = [0,255,0]
+        
+        # cv2.imwrite(f'corners_subpix{i}.png', im)
+
+    """
+	Perform ANMS: Adaptive Non-Maximal Suppression
+	Save ANMS output as anms.png
+	"""
+    
+    features_set = []
+    for i, im in enumerate(corner_im):
+        # lm = ndimage.maximum_filter(im, 20)
+        # mask = (im == lm)
+        # im_set[i][mask] = [0,0,255]
+        
+        
+        
+        cpy = deepcopy(im_set[i])
+        
+        lm = peak_local_max(im, min_distance=2, threshold_rel=0.02)
+        dst = np.zeros(im.shape)
+        features = []
+        for x, y in lm:
+            features.append((y, x))
+            dst[x, y] = 1
+            im_set[i][x, y] = [0, 0, 255]
+            # cv2.circle(im_set[i], (y, x), 1, (0, 0, 255))
+        
+        features_set.append(features)
+        cv2.imwrite(f'outputs/{path}/anms{i}.png', im_set[i])
+            
+        dst = np.uint8(dst)
+        # find centroids
+        ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
+        
+        # define the criteria to stop and refine the corners
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
+        corners = cv2.cornerSubPix(gray,np.float32(centroids),(5,5),(-1,-1),criteria)
+        
+        # Now draw them
+        res = np.hstack((centroids,corners))
+        res = np.int0(res)
+        cpy[res[:,1],res[:,0]]=[0,0,255]
+        cpy[res[:,3],res[:,2]] = [0,255,0]
+        
+        cv2.imwrite(f'corners_subpix{i}.png', cpy)
+
+
+    """
+	Feature Descriptors
+	Save Feature Descriptor output as FD.png
+	"""
+ 
+    def get_patch(image, center, patch_size=(41, 41)):
+        """
+        Extracts a patch around a center pixel in an image.
+        
+        Args:
+            image (numpy.ndarray): Input image.
+            center (tuple): The (x, y) pixel coordinate to center the patch around.
+            patch_size (tuple): Size of the patch, (height, width).
+            
+        Returns:
+            numpy.ndarray: The patch extracted around the given pixel.
+        """
+        # Get patch size (height, width)
+        height, width = patch_size
+        
+        # Calculate the top-left and bottom-right corners of the patch
+        x, y = center
+        top_left_x = max(0, x - width // 2)
+        top_left_y = max(0, y - height // 2)
+        bottom_right_x = min(image.shape[1], x + width // 2 + 1)
+        bottom_right_y = min(image.shape[0], y + height // 2 + 1)
+        
+        # Extract the patch from the image
+        patch = image[top_left_y:bottom_right_y, top_left_x:bottom_right_x]
+        
+        return patch
+    
+    discriptors_set = []
+    keypoints_set = []
+    for i, im in enumerate(im_set):
+        feature_descriptors = []
+        gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+        
+        for x, y in features_set[i]:
+            patch = get_patch(gray, (x, y), patch_size=(41, 41))
+            blur = cv2.GaussianBlur(patch, (5, 5), 0)
+            
+            subsampled = cv2.resize(blur, (8, 8))
+            
+            feature_vector = subsampled.flatten()
+            normalized = (feature_vector - np.mean(feature_vector)) / np.std(feature_vector)
+            
+            feature_descriptors.append(normalized)
+            
+        discriptors_set.append(feature_descriptors)
+            
+
+    """
+	Feature Matching
+	Save Feature Matching output as matching.png
+	"""
+ 
+    def match_features(descriptors1, descriptors2):
+        matches = []
+        for i, descriptor in enumerate(descriptors1):
+            # Sort descriptors in keypoints2 based on their distance to the current descriptor from keypoints1
+            distances = [np.linalg.norm(descriptor - d) for d in descriptors2]
+            sorted_indices = np.argsort(distances)
+            
+            # Apply the ratio test (lowe's ratio test)
+            if distances[sorted_indices[0]] < 0.6 * distances[sorted_indices[1]]:
+                match = cv2.DMatch(i, sorted_indices[0], distances[sorted_indices[0]])
+                matches.append(match)
+        
+        return matches
+    
+    
+    matches = match_features(discriptors_set[0], discriptors_set[1])
+    matches = np.array(matches)
+    
+    # Draw the matches
+    im1 = im_set[0]
+    im2 = im_set[1]
+    
+    key1 = [cv2.KeyPoint(np.float32(x), np.float32(y), 1) for (x, y) in features_set[0]]
+    key2 = [cv2.KeyPoint(np.float32(x), np.float32(y), 1) for (x, y) in features_set[1]]
+    
+    out = cv2.drawMatches(im1, key1, im2, key2, matches, None)
+    
+    cv2.imwrite(f'outputs/{path}/matching.png', out)
+    
+        
+    """
+	Refine: RANSAC, Estimate Homography
+	"""
+ 
+    def get_homography(pairs):
+        for pair in pairs:
+            A = np.array([
+                [-pair[0][0], -pair[0][1], -1, 0, 0, 0, pair[0][0] * pair[1][0], pair[0][1] * pair[1][0], pair[1][0]],
+                [0, 0, 0, -pair[0][0], -pair[0][1], -1, pair[0][0] * pair[1][1], pair[0][1] * pair[1][1], pair[1][1]]
+            ])
+            
+            b = np.array([[-pair[1][0]], [-pair[1][1]]])
+            
+            if 'A_matrix' in locals():
+                A_matrix = np.vstack((A_matrix, A))
+            else:
+                A_matrix = A
+                
+            if 'b_matrix' in locals():
+                b_matrix = np.vstack((b_matrix, b))
+            else:
+                b_matrix = b
+                
+        h = np.linalg.lstsq(A_matrix, b_matrix, rcond=None)[0]
+        h = np.append(h, 1)
+
+        return h.reshape(3, 3)
+        
+    def RANSAC(keypoints1, keypoints2, matches, tau, N):
+        max_inliers = 0
+        best_homography = None
+        for i in range(N):
+            # Randomly select 4 pairs
+            random_pairs = np.random.choice(matches, 4, replace=False)
+            pairs = [[keypoints1[pair.queryIdx], keypoints2[pair.trainIdx]] for pair in random_pairs]
+            
+            # Compute homography
+            H = get_homography(random_pairs)
+            
+            
+
+    """
+	Image Warping + Blending
+	Save Panorama output as mypano.png
+	"""
+
+
+if __name__ == "__main__":
+    main()
